bc4bfc418700a92a29692f2e4c7d3b6bc5bed66d
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-08-22 14:48:23 +0300

    up

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../../images/logo.png'
 import cartIcon from "../../images/cartIcon.png";
 import Image from 'next/image'
@@ -6,18 +6,33 @@ import Link from 'next/link'
 import { useSelector, useDispatch } from "react-redux";
 import { SlLocationPin } from "react-icons/sl";
 import { HiOutlineSearch } from "react-icons/hi";
-import { BiCaretDown } from "react-icons/bi";
-import { StateProps } from '../../../type';
+import { StateProps, StoreProduct } from '../../../type';
 import { MdOutlineFavorite } from "react-icons/md";
+import SearchProducts from '../SearchProducts';
 const Header = () => {
-  const { productData, favoriteData } = useSelector((state: StateProps) => state.next)
-  console.log(productData)
-  console.log("this is favourit", favoriteData)
+  const { productData, favoriteData } = useSelector((state: StateProps) => state.next
+  );
 
   //serach bar
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filterProduct, setFilterProduct] = useState("")
+  const [searchQuery, setSearchQuery] = useState(""); // User input for search
+  const [filteredProducts, setFilteredProducts] = useState([]); // Filtered products based on search
+  const [allData, setAllData] = useState([]); // All products data
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("Search query:", searchQuery);
+    console.log("All data:", allData);
+
+    const filtered = allData.filter((item: StoreProduct) =>
+      item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    console.log("Filtered data:", filtered);
+
+    setFilteredProducts(filtered);
+  }, [searchQuery, allData]);
   return (
     <div className='w-full
     sm:max-w-screen-2xl
@@ -39,16 +54,58 @@ const Header = () => {
           </div>
         </div>
         {/*searchbar*/}
-        <div className='flex-1 h-10 hidden md:inline-flex items-center justify-between relative'>
-          <input type="text"
+        <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative">
+          <input
+            onChange={handleSearch}
+            value={searchQuery}
             className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none focus-visible:border-amazon_yellow"
-
-            placeholder='Search products in Planing Soultion' />
+            type="text"
+            placeholder="Search Planing soultion products"
+          />
           <span className="w-12 h-full bg-amazon_yellow text-black text-2xl flex items-center justify-center absolute right-0 rounded-tr-md rounded-br-md">
             <HiOutlineSearch />
           </span>
+          {/* ========== Searchfield ========== */}
+          {searchQuery && (
+            <div className="absolute left-0 top-12 w-full mx-auto max-h-96 bg-gray-200 rounded-lg overflow-y-scroll cursor-pointer text-black">
+              {filteredProducts.length > 0 ? (
+                <>
+                  {searchQuery &&
+                    filteredProducts.map((item: StoreProduct) => (
+                      <Link
+                        key={item._id}
+                        className="w-full border-b-[1px] border-b-gray-400 flex items-center gap-4"
+                        href={{
+                          pathname: `${item._id}`,
+                          query: {
+                            _id: item._id,
+                            brand: item.brand,
+                            category: item.category,
+                            description: item.description,
+                            image: item.image,
+                            isNew: item.isNew,
+                            oldPrice: item.oldPrice,
+                            price: item.price,
+                            title: item.title,
+                          },
+                        }}
+                        onClick={() => setSearchQuery("")}
+                      >
+                        <SearchProducts key={item._id} item={item} />
+                      </Link>
+                    ))}
+                </>
+              ) : (
+                <div className="bg-gray-50 flex items-center justify-center py-10 rounded-lg shadow-lg">
+                  <p className="text-xl font-semibold animate-bounce">
+                    Nothing is matches with your search keywords. Please try
+                    again!
+                  </p>
+                </div>
+              )}
+            </div>
+          )}
         </div>
-
         {/*favourit*/}
         <Link href={"/favorite"}
           className="text-xs text-gray-100 flex flex-col justify-center px-3 border
